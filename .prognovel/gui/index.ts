74b398fb158823a248d8c7807bf6b39afab2d307
ProgNovel <prognovel@gmail.com>
2022-01-13 15:02:42 +0800

    send settings data on receiving ws message

--- .prognovel/gui/index.ts
+++ .prognovel/gui/index.ts
@@ -1,4 +1,7 @@
 import WebSocket from "ws";
+import { readFileSync } from "fs";
+import { siteFiles } from "../_files";
+import { load } from "js-yaml";
 
 export function initializeGUIServer() {
   console.log("...\n");
@@ -10,8 +13,31 @@ export function initializeGUIServer() {
     Admin page is now ready to use in www.<your-prognovel-site>.com/admin`);
 
   wss.on("connection", (ws) => {
-    ws.on("message", WebSocketMessage);
+    ws.on("message", (d: Buffer) => {
+      try {
+        const data = JSON.parse(d.toString("utf-8"));
+        console.log("incoming:", data);
+
+        if (data.type === "FETCH") fetchFile(ws, data.file);
+      } catch (error) {
+        console.error(error);
+      }
+    });
   });
 }
 
-function WebSocketMessage(data: any) {}
+async function fetchFile(ws: WebSocket, file: string) {
+  let data;
+  let path = "";
+  let novel = "";
+  if (file.split("/").length > 2) {
+    [novel, file] = file.split("/");
+  }
+
+  if (novel) {
+  } else {
+    data = load(readFileSync(siteFiles()[file], "utf-8"));
+  }
+
+  ws.send(JSON.stringify({ type: "FETCH", file, data }));
+}
