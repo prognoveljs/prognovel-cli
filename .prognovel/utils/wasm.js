b24e2b28ec09c5720bc60328ce12193ae93e6979
ProgNovel <prognovel@gmail.com>
2022-01-18 10:30:23 +0800

    hotfix wasm build error

--- .prognovel/utils/wasm.js
+++ .prognovel/utils/wasm.js
@@ -1,9 +1,10 @@
-import { copyFileSync } from "fs";
+import { copyFileSync, existsSync, mkdirSync } from "fs";
 
 export function markdownWasmOutput() {
   return {
     name: "markdown-wasm-output",
     buildEnd() {
+      if (!existsSync(".prognovel/.dist")) mkdirSync(".prognovel/.dist", { recursive: true });
       console.log("🌟 copying markdown-wasm file...");
       copyFileSync("node_modules/markdown-wasm/dist/markdown.wasm", ".prognovel/.dist/markdown.wasm");
     },
